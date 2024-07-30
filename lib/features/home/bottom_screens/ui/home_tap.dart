12131906088377e7c1dfff9f7def9eae7f212ee1
tamtawy <ahmedtramadan4@gmail.com>
2024-07-30 16:30:59 +0300

    - Start craete Home Screen - added a home,profile,search Taps. - some edits in sign up form.

---
+++ lib/features/home/bottom_screens/ui/home_tap.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeTap extends StatelessWidget {
+  const HomeTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.cyan,
+      body: Center(
+        child: Text("Home Tap"),
+      ),
+    );
+  }
+}
