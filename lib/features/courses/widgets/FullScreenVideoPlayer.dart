642f2b14b794712ef806582a480b04a0270dc903
tamtawy <ahmedtramadan4@gmail.com>
2024-09-05 13:26:05 +0300

    fix some bugs in zooming video in full screen fix download assets

--- lib/features/courses/widgets/FullScreenVideoPlayer.dart
+++ lib/features/courses/widgets/FullScreenVideoPlayer.dart
@@ -14,21 +14,44 @@ class FullScreenVideoPlayer extends StatefulWidget {
 }
 
 class _FullScreenVideoPlayerState extends State<FullScreenVideoPlayer> {
+  bool isFullScreen = false; // Track fullscreen mode
+
   @override
   void initState() {
     super.initState();
     _disableScreenRecording();
+
+    // Listen for fullscreen changes
+    widget.controller.addListener(() {
+      if (widget.controller.value.isFullScreen != isFullScreen) {
+        setState(() {
+          isFullScreen = widget.controller.value.isFullScreen;
+        });
+
+        // Adjust the orientation when entering or exiting fullscreen
+        if (isFullScreen) {
+          SystemChrome.setPreferredOrientations([
+            DeviceOrientation.landscapeRight,
+            DeviceOrientation.landscapeLeft,
+          ]);
+        } else {
+          SystemChrome.setPreferredOrientations([
+            DeviceOrientation.portraitUp,
+            DeviceOrientation.portraitDown,
+          ]);
+        }
+      }
+    });
   }
 
   @override
   void dispose() {
-    // Set the preferred orientation to landscape before navigating back
+    // Reset orientation to portrait before disposing
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
 
-    // Dispose of the YoutubePlayerController
     widget.controller.dispose();
     super.dispose();
   }
@@ -37,13 +60,20 @@ class _FullScreenVideoPlayerState extends State<FullScreenVideoPlayer> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: YoutubePlayer(
-          controller: widget.controller,
-          showVideoProgressIndicator: true,
-          progressIndicatorColor: Colors.red,
-          progressColors: const ProgressBarColors(
-            playedColor: ColorsCode.mainBlue,
-            handleColor: ColorsCode.darkBlue,
+        child: Transform.scale(
+          scale: isFullScreen ? 0.84: 1.0, // Zoom in when fullscreen
+          child: YoutubePlayer(
+            controller: widget.controller,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.red,
+            progressColors: const ProgressBarColors(
+              playedColor: ColorsCode.mainBlue,
+              handleColor: ColorsCode.darkBlue,
+            ),
+            onReady: () {
+              // Automatically enter fullscreen when video starts
+              widget.controller.toggleFullScreenMode();
+            },
           ),
         ),
       ),
